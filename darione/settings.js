465c0cb0790f69e2065349c4f9aff9eea0194fff
Andrea Gallegati <37024974+andreagalle@users.noreply.github.com>
2024-02-21 18:05:15 +0100

    first walkintiber deploy

---
+++ darione/settings.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1955177.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 168339172, 168339383, 168339517 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/168339204/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/168339205/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/168339203/1/ammo.js', 'preload' : true},
+];
